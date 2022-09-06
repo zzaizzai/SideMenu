1024fd8c6ac4705367f9e2c5b74b26f3e222014d
junsai <junsai0530@gmail.com>
2022-09-07 00:16:00 +0900

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by 小暮準才 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
